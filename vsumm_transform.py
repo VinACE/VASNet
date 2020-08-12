439ee06999c376021c7be016e824f958f6a52691
vinace <p.vinsentds@gmail.com>
2020-08-12 17:57:06 +0530

    Enabling tracing

--- vsumm_transform.py
+++ vsumm_transform.py
@@ -236,6 +236,7 @@ class Transformer(nn.Module):
         return out
     
 if __name__ == "__main__":
+    import pdb;pdb.set_trace()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     x = torch.tensor([[1, 5, 6, 4, 3, 9, 5, 2, 0], [1, 8, 7, 3, 4, 5, 6, 7, 2]]).to(
         device
