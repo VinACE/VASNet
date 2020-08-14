2054140519ea84ea8f0a68f52f0894b0fe889a63
vinace <p.vinsentds@gmail.com>
2020-08-14 08:43:00 +0530

    Attention code for weights added

--- tvsumm_transform.py
+++ tvsumm_transform.py
@@ -112,14 +112,14 @@ class Encoder(nn.Module):
         )
         self.dropout = nn.Dropout(dropout)
     
-    def forward(self, x, mask):
+    def forward(self, x, mask,):
         N, seq_length = x.shape
         positions = torch.arange(0, seq_length).expand(N, seq_length).to(self.device)
         
         out = self.dropout(self.word_embedding(x) + self.position_embedding(positions)) ## Need to understand what is positions..
         weights = []
         for layers in self.layers:
-            out, weight = layers(out, out, out, mask)
+            out, weight = layers(out, out, out, attn_mask=mask, )
             weights.append(weight)
 
 
