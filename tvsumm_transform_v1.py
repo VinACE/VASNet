89abfa0c7f450287c45de186b6704d4bc031cdbc
vinace <p.vinsentds@gmail.com>
2020-08-17 16:02:28 +0530

    Added weights related code

--- tvsumm_transform_v1.py
+++ tvsumm_transform_v1.py
@@ -1,44 +1,28 @@
 """
 # TODO Multihead implementation https://github.com/dreamgonfly/Transformer-pytorch/blob/master/models.py
-https://buomsoo-kim.github.io/attention/2020/04/27/Attention-mechanism-21.md/
-https://github.com/bentrevett/pytorch-seq2seq/blob/master/6%20-%20Attention%20is%20All%20You%20Need.ipynb
-
-# Display attention mechanism.. shown in the above link for the paper..
 """
 
 import torch
 import torch.nn as nn
 
 class SelfAttention(nn.Module):
-    def __init__(self, embed_size, heads): #  heads=8
+    def __init__(self, embed_size, heads ): #  heads=8
         super(SelfAttention, self).__init__()
         self.embed_size = embed_size
         self.heads = heads
         self.head_dim = embed_size // heads
-        self.input_size=10
-        self.output_size=10
-        self.m = self.input_size        
+
         assert (self.head_dim * heads == embed_size), "Embed size needs to be divisible by head size"
 
-        self.values = nn.Linear(in_features=self.m, out_features=self.output_size, bias=False)
-        self.keys = nn.Linear(in_features=self.m, out_features=self.output_size, bias=False) 
-        self.queries = nn.Linear(in_features=self.m, out_features=self.output_size, bias=False)        
-        self.fc_out = nn.Linear(in_features=self.output_size, out_features=self.m, bias=False)
-
-        apperture=-1
-        ignore_itself=False
-        self.apperture = apperture
-        self.ignore_itself = ignore_itself
+        self.values = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.keys = nn.Linear(self.head_dim, self.head_dim, bias=False) 
+        self.queries = nn.Linear(self.head_dim, self.head_dim, bias=False)        
+        self.fc_out = nn.Linear(heads * self.head_dim, embed_size)
+    
 
     def forward(self, values, keys, query, mask):
-        N = x.shape[0]
-        # try:
-        #     if query.shape[0] is not None:
-        #         # import pdb;pdb.set_trace()
-            # mask = None
-            
-        value_len, key_len, query_len = x.shape[1], x.shape[1], x.shape[1]
-        
+        N = query.shape[0]
+        value_len, key_len, query_len = values.shape[1], keys.shape[1], query.shape[1]
 
         # split embedding into self. head pieces
         values = values.reshape(N, value_len, self.heads, self.head_dim)
@@ -47,13 +31,7 @@ class SelfAttention(nn.Module):
 
         values = self.values(values)
         keys = self.keys(keys)
-        queries = self.queries(query)
-        
-        queries *= 0.06
-        logits = torch.matmul(queries, keys.transpose(1,0))
-        if self.ignore_itself:
-            # Zero the diagonal activations (a distance of each frame with itself)
-            logits[torch.eye(N).byte()] = -float("Inf")
+        queries = self.queries(queries)
         
         energy = torch.einsum("nqhd,nkhd->nhqk", [queries, keys])
         # queries shape : (N, query_len, heads, heads_dim)
@@ -63,23 +41,19 @@ class SelfAttention(nn.Module):
         if mask is not None:
             energy =  energy.masked_fill(mask == 0, float("-1e20")) # for numerical stability
         
-        attention = torch.softmax(energy / (self.embed_size ** (1/2)), dim=-1) # Attention(Q,K,V) = sofmax(QK^{T}/(d_{k})**(1/2)) * V
+        attention = torch.softmax(energy / (self.embed_size ** (1/2)), dim=3) # Attention(Q,K,V) = sofmax(QK^{T}/(d_{k})**(1/2)) * V
 
         out = torch.einsum("nhql,nlhd->nqhd", [attention, values]).reshape(
             N, query_len, self.heads * self.head_dim
         )
-
         # Attention shape: (N, heads, query_len, key_len)
         # value shape: (N, Value_len, heads, heads_dim) key length and the value lenth are alwasy going to be the same.
         # after einsum (N, query_len, heads, head_dim) flatten last two dimension..
-        
-        weights = self.drop50(attention)
 
         out = self.fc_out(out)
 
-        return out, weights
-    # except:
-    #     pass
+        return out, attention
+
 
 class TransformerBlock(nn.Module):
     def __init__(self, embed_size, heads, dropout, forward_expansion):
@@ -96,12 +70,12 @@ class TransformerBlock(nn.Module):
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, value, key, query, mask):
-        attention, _ = self.attention(value, key, query, mask) # TODO check this place if weights _ is required are not..
+        attention = self.attention(value, key, query, mask)
 
         x = self.dropout(self.norm1(attention + query))
         forward = self.feed_forward(x)
         out = self.dropout(self.norm2(forward + x))
-        yield out
+        return out
 
 class Encoder(nn.Module):
     def __init__(
@@ -133,18 +107,18 @@ class Encoder(nn.Module):
         )
         self.dropout = nn.Dropout(dropout)
     
-    def forward(self, x, mask,):
+    def forward(self, x, mask):
         N, seq_length = x.shape
         positions = torch.arange(0, seq_length).expand(N, seq_length).to(self.device)
         
         out = self.dropout(self.word_embedding(x) + self.position_embedding(positions)) ## Need to understand what is positions..
-        
+
         for layers in self.layers:
-            out = layers(out, out, out, mask, )
-            
-        return out # should we return the weights in Encoder.. or is it ok only to return on the Decoder part...
-# DECODER LAYER..    
-class DecoderBlock(nn.Module):         
+            out = layers(out, out, out, mask)
+        
+        return out  # should we return the weights in Encoder.. or is it ok only to return on the Decoder part...
+    
+class DecoderBlock(nn.Module):
     def __init__(self, embed_size, heads, forward_expansion, dropout, device):
         super(DecoderBlock, self).__init__()
         self.attention = SelfAttention(embed_size, heads)
@@ -156,13 +130,13 @@ class DecoderBlock(nn.Module):
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x, value, key, src_mask, trg_mask):  # x  & V & K are comming in from the encoder..
-        attention, weights = self.attention(x, x, x, trg_mask)  # ENC (n x m) => (n x H)
+        attention = self.attention(x, x, x, trg_mask)  # ENC (n x m) => (n x H)
         query = self.dropout(self.norm(attention + x))
         out = self.transformer_block(value, key, query, src_mask)
-        return out, weights
+        return out
 
     
-class Decoder(nn.Module):    ## DECODER BLOCK
+class Decoder(nn.Module):
     def __init__(
                     self, 
                     trg_vocab_size,
@@ -193,11 +167,11 @@ class Decoder(nn.Module):    ## DECODER BLOCK
         x = self.dropout((self.word_embedding(x) + self.position_embedding(positions)))
 
         for layer in self.layers:
-            x, weights = layer(x, enc_out, enc_out, src_mask, trg_mask)
+            x = layer(x, enc_out, enc_out, src_mask, trg_mask)
 
         out = self.fc_out(x)
 
-        return out, weights
+        return out
 
 class Transformer(nn.Module):
     def __init__(
@@ -258,17 +232,16 @@ class Transformer(nn.Module):
         src_mask = self.make_src_mask(src)
         trg_mask = self.make_trg_mask(trg)
         enc_src = self.encoder(src, src_mask)
-        out = self.decoder(trg, enc_src, src_mask, trg_mask) # TODO weight should come here as well
+        out = self.decoder(trg, enc_src, src_mask, trg_mask)
         return out
     
 if __name__ == "__main__":
-    # import pdb;pdb.set_trace()
+    import pdb;pdb.set_trace()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # device = torch.device("cpu")
     x = torch.tensor([[1, 5, 6, 4, 3, 9, 5, 2, 0], [1, 8, 7, 3, 4, 5, 6, 7, 2]]).to(
         device
     )
-    trg =  torch.tensor([[1, 7, 4, 3, 5, 9, 2, 0, 0,0], [1, 5, 6, 2, 4, 7, 6, 2,0,0]]).to(device)
+    trg =  torch.tensor([[1, 7, 4, 3, 5, 9, 2, 0], [1, 5, 6, 2, 4, 7, 6, 2]]).to(device)
 
     src_pad_idx = 0
     trg_pad_idx = 0
@@ -277,8 +250,5 @@ if __name__ == "__main__":
     model = Transformer(src_vocab_size, trg_vocab_size, src_pad_idx, trg_pad_idx).to(
         device
     )
-    out, weights = model(x, trg[:, :-1])
-    # weight_mat_ = model(x, trg[:, :-1])[1]
-    print(out)
-    # print("**************")
-    # print(weight_mat_)
+    out = model(x, trg[:, :-1])
+    print(out.shape)
