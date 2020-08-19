83b47b3f4cb8a788cec5b12476ea31e8f3a6ca76
vinace <p.vinsentds@gmail.com>
2020-08-19 23:31:37 +0530

    Enabling debugging

--- seq_sum.py
+++ seq_sum.py
@@ -454,17 +454,6 @@ if __name__ == "__main__":
 
     src_vocab_size = 10
     trg_vocab_size = 10
-    # model = Seq2Seq(enc, dec, SRC_PAD_IDX, TRG_PAD_IDX, device).to(device)
-
-
-    #     out, attention = model(x, trg[:, :-1])
-    #     print(out.shape)
-
-
-    # x = torch.tensor([['1','5','6','4','3','9','5','2','0','0' ] , ['1','8','7','3','4','5','6','7','2','0']]).to(
-    #     device
-    # )
-    # trg =  torch.tensor([['1','7','4','3','5','9','2','0','1'], ['1','5','6','2','4','7','6','2','0']]).to(device)
 
     x = torch.tensor([[1, 5, 6, 4, 3, 9, 5, 2, 0, 0], [1, 8, 7, 3, 4, 5, 6, 7, 2, 0]]).to(
         device, dtype=torch.int64
